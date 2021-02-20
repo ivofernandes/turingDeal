b360ec5ad0ea509d11841d43407bb8e4b9346894
Ivo Fernandes <ivomuze@gmail.com>
2021-02-20 23:45:46 +0000

    Way of cache daframes in the local machine

--- src/aux/utils/cache.py
+++ src/aux/utils/cache.py
@@ -1,18 +1,45 @@
+import os
+from sys import path
+
 import pandas as pd
 from pathlib import Path
-import os
+from os import path
 
-# You should change 'test' to your preferred folder.
-
-def saveDataframe(dataframe, folder, ticker):
+def getFilePath(folder, ticker):
+    # Get the file path
     script_location = Path(__file__).absolute().parent
 
     cacheFolderLocation = '../../../cache/'
     folderLocation = cacheFolderLocation + folder + '/'
     fileRelativeLocation = folderLocation + ticker + '.csv'
 
+    file_location = script_location / fileRelativeLocation
+    file_path = '/'.join(file_location.parts)
+
+    # Guarantee that the folders exists
     os.makedirs(script_location / cacheFolderLocation, exist_ok=True)
     os.makedirs(script_location / folderLocation, exist_ok=True)
 
-    file_location = script_location / fileRelativeLocation
-    dataframe.to_csv(file_location)
+    return file_path
+
+# Check if a file exists
+def existsFile(folder, ticker):
+    file_path = getFilePath(folder, ticker)
+    exists = path.exists(file_path)
+    return exists
+
+# Save dataframe to a cache folder
+def saveDataframe(dataframe, folder, ticker):
+
+    file_path = getFilePath(folder, ticker)
+    dataframe.to_csv(file_path)
+
+# Load daframe from a cache folder
+def loadDataframe(folder, ticker):
+    file_path = getFilePath(folder, ticker)
+
+    exists = path.exists(file_path)
+
+    if exists:
+        dataframe = pd.read_csv(file_path)
+        return dataframe
